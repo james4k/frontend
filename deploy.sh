fc8815857494dfb2917b3d5e156a4e6372eafa62
James Gray <james@james4k.com>
2014-05-26 19:41:02 -0500

    remove old generated assets

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ set -e
 
 mkdir -p bin
 GOOS=linux GOARCH=amd64 go build -o ./bin/frontend -tags prod .
+rm -f content/assets/script-*.js content/assets/style-*.css
 mend -f content/mend.json -o content/mend-versions.json content
 cd admin
 ansible-playbook -i prod deploy.yml
