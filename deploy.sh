e9ba3d83737c78606c8bf6ac893d1ad0a906c416
James Gray <james@james4k.com>
2014-05-26 20:09:40 -0500

    clean only old assets, keeping the two newest
    Just to be browser cache friendly...

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@ set -e
 
 mkdir -p bin
 GOOS=linux GOARCH=amd64 go build -o ./bin/frontend -tags prod .
-rm -f content/assets/script-*.js content/assets/style-*.css
+ls -t content/assets/script-*.js | awk 'NR>2' | xargs -L 1 rm
+ls -t content/assets/style-*.css | awk 'NR>2' | xargs -L 1 rm
 mend -f content/mend.json -o content/mend-versions.json content
 cd admin
 ansible-playbook -i prod deploy.yml
